e693fb1e40e6b2046ed9d56365d89b94080f365e
dattesuki <dattesuki@mail.ru>
2025-03-10 02:31:24 +0300

    update

--- Gauss–Seidel.cpp
+++ Gauss–Seidel.cpp
@@ -2,12 +2,12 @@
 #include "tmatrix.h"
 #include <string>
 #include <time.h>
-#include <algorithm>
+
 
 /*
 //Ax = b
 template<typename type>
-TDynamicVector<type> Gauss_Seidel_it(TDynamicMatrix<type> A, TDynamicVector<type> b, TDynamicVector<type> x, size_t num) {
+TDynamicVector<type> Gauss_Seidel_it(TDynamicMatrix<type> A, TDynamicVector<type> x, TDynamicVector<type> b, size_t num) {
 	TDynamicVector<type> temp(b.size());
 	temp = x;
 	for (size_t n = 0; n < num; ++n) {
@@ -31,7 +31,7 @@ TDynamicVector<type> Gauss_Seidel_it(TDynamicMatrix<type> A, TDynamicVector<type
 */
 
 template<typename type>
-TDynamicVector<type> Gauss_Seidel_type(TDynamicMatrix<type> A, TDynamicVector<type> b, TDynamicVector<type> x, type ref = 1.e-8) {
+TDynamicVector<type> Gauss_Seidel_accurate(TDynamicMatrix<type> A, TDynamicVector<type> x, TDynamicVector<type> b, type ref = 1.e-8) {
 	TDynamicVector<type> temp(b.size());
 
 	//initial approximation
@@ -57,6 +57,42 @@ TDynamicVector<type> Gauss_Seidel_type(TDynamicMatrix<type> A, TDynamicVector<ty
 	}
 	return x;
 }
+
+template<typename type>
+TDynamicVector<type> Gauss_Seidel_close(TDynamicMatrix<type> A, TDynamicVector<type> x, TDynamicVector<type> b, type ref = 1.e-8) {
+	TDynamicVector<type> temp(b.size());
+
+	//initial approximation
+	temp = x;
+	bool flag;
+	size_t count_it = 0;
+	while (!CloseSol(A, x, b, ref)) {
+		for (size_t i = 0; i < b.size(); ++i) {
+			x[i] = b[i];
+
+			for (size_t j = 0; j < i; ++j) {
+				x[i] -= A[i][j] * x[j];
+			}
+
+			for (size_t j = i + 1; j < b.size(); ++j) {
+				x[i] -= A[i][j] * temp[j];
+			}
+
+			x[i] *= (1 / A[i][i]);
+		}
+
+		flag = true;
+		for (size_t i = 0; i < x.size(); ++i) if (abs(x[i] - temp[i]) >= ref) flag = false;
+		if (flag == true) break;
+
+		temp = x;
+		count_it++;
+	}
+	return x;
+}
+
+
+
 
 /*
 TDynamicVector<double> Gauss_Seidel(TDynamicMatrix<double> A, TDynamicVector <double> b, size_t it_count, double ref = 1.e-8) {
@@ -96,18 +132,13 @@ TDynamicVector<double> Gauss_Seidel(TDynamicMatrix<double> A, TDynamicVector <do
 
 	A_fp32 = TDynamicMatrix<float>(A);
 	b_fp32 = TDynamicVector<float>(b);
-	x_fp32 = Gauss_Seidel_type(A_fp32, b_fp32, x_fp32, float(ref1));
+	x_fp32 = Gauss_Seidel_close(A_fp32, x_fp32, b_fp32, float(ref1));
 
 
-	TDynamicMatrix<double> A_fp64(A.size());
 	TDynamicVector<double> x_fp64(b.size());
-	TDynamicVector<double> b_fp64(b.size());
-
-	A_fp64 = TDynamicMatrix<float>(A_fp32);
-	b_fp64 = TDynamicVector<float>(b_fp32);
 	x_fp64 = TDynamicVector<float>(x_fp32);
 
-	x_fp64 = Gauss_Seidel_type(A_fp64, b_fp64, x_fp64, ref2);
+	x_fp64 = Gauss_Seidel_accurate(A, x_fp64, b, ref2);
 
 	return x_fp64;
 }
@@ -115,50 +146,61 @@ TDynamicVector<double> Gauss_Seidel(TDynamicMatrix<double> A, TDynamicVector <do
 
 int main() {
 	time_t t,t1,t2,t3,t4,t5;
-	TDynamicMatrix<double> A(600);
+	TDynamicMatrix<double> A(1000);
 	TDynamicVector<double> b(A.size());
 	TDynamicVector<double> x(A.size());
-
-
-	for (size_t i = 0; i < 100; ++i) {
+	vector<double> VectorTime= { 0,0,0,0,0};
+	vector<int> VectorTimeCount = { 0,0,0,0,0 };
+	for (size_t i = 0; i < 5; ++i) {
 		A.generateGoodMatrix();
 		//A = generateGoodMatrix<double>(A.size());
 		b.generate();
 
+		t = clock();
+		x = Gauss_Seidel(A, b, 1.e-2, 1.e-10);
+		t -= clock();
+		t1 = t *= -1;
+		VectorTime[0] = double(t1);
+		cout << double(t) << ", ";
+
+		t = clock();
+		x = Gauss_Seidel(A, b, 1.e-3, 1.e-10);
+		t -= clock();
+		t2 = t *= -1;
+		VectorTime[1] = double(t2);
+		cout << double(t) <<", ";
 
 		t = clock();
 		x = Gauss_Seidel(A, b, 1.e-4, 1.e-10);
 		t -= clock();
-		t1 = t *= -1;
-		cout << double(t) <<", ";
+		t3=t *= -1;
+		VectorTime[2] = double(t3);
+		cout << double(t) << ", ";
+
 
 		t = clock();
 		x = Gauss_Seidel(A, b, 1.e-5, 1.e-10);
 		t -= clock();
-		t2=t *= -1;
+		t4=t *= -1;
+		VectorTime[3] = double(t4);
 		cout << double(t) << ", ";
 
 
 		t = clock();
 		x = Gauss_Seidel(A, b, 1.e-6, 1.e-10);
 		t -= clock();
-		t3=t *= -1;
-		cout << double(t) << ", ";
-
-
-		t = clock();
-		x = Gauss_Seidel(A, b, 1.e-7, 1.e-10);
-		t -= clock();
-		t4=t *= -1;
-		cout << double(t) << ", ";
-
-		t = clock();
-		x = Gauss_Seidel(A, b, 1.e-8, 1.e-10);
-		t -= clock();
 		t5=t *= -1;
+		VectorTime[4] = double(t5);
 		cout << double(t) << endl;
+		//cout << (A * x - b) << endl;
 
+
+		VectorTimeCount[min_element(VectorTime.begin(), VectorTime.end()) - VectorTime.begin()]++;
+		
+
+		
 	}
+	cout << VectorTimeCount[0] << " "<<VectorTimeCount[1] << " "<<VectorTimeCount[2] << " "<<VectorTimeCount[3] << " " << VectorTimeCount[4] << endl;
 	return 0;
 }
 
